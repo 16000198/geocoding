fa9a0c0efa2a037c6d68d6e2e88ae91bddde85b7
sildvauresberg <slickunit4265752@gmail.com>
2020-05-24 04:26:12 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,26 @@
 subprojects {
 	group = "io.slick.metcast"
 	version = "1.0"
+	plugins {
+		maven-publish
+	}
 	repositories {
 		mavenCentral()
+		maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/16000198/geocoding")
+            credentials {
+                username = System.getenv("GITHUB_TOKEN")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
 	}
 	tasks.withType<JavaCompile> {
 		options.encoding = "UTF-8"
 	}
+	publications {
+        register("gpr") {
+            from(components["java"])
+        }
+    }
 }
